481e8b08c4ed35401b3fc2c35c1f54ed699a7968
shamzoro <shamvijayakumar2006@gmail.com>
2025-12-23 11:02:20 +0530

    Counter done and tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Count from './components/count'
 
 function App() {
   const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Spider Man</h1>
-      <h2>Peter Parker</h2>
-    </>
-  )
+return(
+  <div>
+  <Count/>
+  </div>
+)
 }
 
 export default App
